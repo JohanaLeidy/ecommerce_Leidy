8f234e662aeb4099bf1cacecea1c4fad24bd09e0
Leidy Jhoanna <leidydelgadillo201@gmail.com>
2022-09-28 09:31:42 -0500

    agregando los templates

---
+++ store/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Product
+
+def store(request):
+	products = Product.objects.all()
+	context = {'products':products}
+	return render(request, 'store/store.html', context)
+
+def cart(request):
+ context = {}
+ return render(request, 'store/cart.html', context)
+ 
+def checkout(request):
+ context = {}
+ return render(request, 'store/checkout.html', context)
